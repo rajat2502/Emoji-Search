20c8273afcc1b39dc687d3750336d2042d20ab82
Rajat Verma <rajatverma5885045@gmail.com>
2019-11-03 22:37:47 +0530

    Updates

---
+++ precache-manifest.064538ac5d5fb4faa15e9c7e8172bdba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c15bbe26aa52c69ab751f0a74d8590",
+    "url": "/Emoji-Search/index.html"
+  },
+  {
+    "revision": "6b295f5fd0b2b4ac2cba",
+    "url": "/Emoji-Search/static/css/main.b406624a.chunk.css"
+  },
+  {
+    "revision": "f6d8b7fb1c14d4101928",
+    "url": "/Emoji-Search/static/js/2.6c4a6e7c.chunk.js"
+  },
+  {
+    "revision": "6b295f5fd0b2b4ac2cba",
+    "url": "/Emoji-Search/static/js/main.c2544ce4.chunk.js"
+  },
+  {
+    "revision": "cd282d6f9bb25d724dd1",
+    "url": "/Emoji-Search/static/js/runtime-main.fc42c971.js"
+  }
+]);
